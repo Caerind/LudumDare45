7f4a561cbe1f02412a281d425a3983b21800fa2d
Cmdu76 <charles.mailly@free.fr>
2020-01-05 00:42:05 +0100

    Oops remove forgotten things

--- tests/Math/Vector3_Tests.cpp
+++
@@ -1,8 +0,0 @@
-#include <Enlivengine/Math/Vector3.hpp>
-
-#include <doctest/doctest.h>
-
-DOCTEST_TEST_CASE("Vector3")
-{
-	// TODO : Vector3 tests
-}
--- tests/Math/Vector4_Tests.cpp
+++
@@ -1,8 +0,0 @@
-#include <Enlivengine/Math/Vector4.hpp>
-
-#include <doctest/doctest.h>
-
-DOCTEST_TEST_CASE("Vector4")
-{
-	// TODO : Vector4 tests
-}
